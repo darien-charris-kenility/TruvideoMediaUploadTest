01074cbedec8b59c7c38f1c0a89090fb77c3030a
Jorge Orjuela <111378570+jorge-jcrew@users.noreply.github.com>
2026-02-11 11:34:37 -0500

    x

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MediaUploadBinary",
-            url: "https://github.com/darien-charris-kenility/TruvideoMediaUploadTest/releases/download/0.1.6/TruvideoMediaUploadTest.xcframework.zip",
+            url: "https://github.com/darien-charris-kenility/TruvideoMediaUploadTest/releases/download/0.1.6/MediaUpload.xcframework.zip",
             checksum: "bd222321b004d6b8065fa3c57322808d0caf7d69c3de68c4df780e00c3a1d9db"
         ),
         .target(
